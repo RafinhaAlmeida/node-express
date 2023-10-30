392c16bfd777244fb22d360f602d18c198c804a4
EA - Rafael Rodrigues De Almeida <rafael_almeida01@educadventista.org>
2023-10-30 15:05:07 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`acessando dados do usuario ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
-    resposta.sendFile(`{caminhoBase}/index.html`)
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
